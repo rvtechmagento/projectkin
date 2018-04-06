35055d4edcc2f9c0d9c68901c913fb16592fe642
ashishrvtech <ashish_sharma@rvtechnologies.co.in>
2018-04-06 14:19:17 +0530

    test

--- app/design/frontend/Mykin/mykininc/registration.php
+++ app/design/frontend/Mykin/mykininc/registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Mykin/mykininc',
+    'frontend/Mykin/ma_carstore',
     __DIR__
 );
